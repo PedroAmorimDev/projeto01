ca4c94435dc47199fc3d1adbd6df8d0898f029a4
Pedro Henrique <138811347+PedroAmorimDev@users.noreply.github.com>
2024-03-04 15:18:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const url = "https://api.chucknorris.io/jokes/categories";
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao receber os dados");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      gerarCategorias(data);
+    })
+    .catch((err) => console.log(err));
+});
+
+function gerarCategorias(categorias) {
+  const select = document.getElementById("select");
+
+  categorias.map((categoria) => {
+    const option = document.createElement("option");
+    option.innerHTML = `${categoria}`;
+    option.value = categoria;
+    select.appendChild(option);
+  });
+}
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  const select = document.getElementById("select").value;
+
+  const url = `https://api.chucknorris.io/jokes/random?category=${select}`;
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao receber os dados");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      criarPiada(data);
+    })
+    .catch((err) => console.log(err));
+});
+
+function criarPiada(piada) {
+  const joke = document.getElementById('joke');
+  joke.innerHTML = piada.value;
+}
